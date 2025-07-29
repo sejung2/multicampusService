01d09ef373dfc129835f35b49853e843dc66ff95
sejung2 <seingdev@gmail.com>
2025-07-29 17:56:31 +0900

    feat: class & constructor

--- java-app/src/oopClass/sec05/Tv.java
+++ java-app/src/oopClass/sec05/Tv.java
@@ -2,8 +2,8 @@ package oopClass.sec05;
 
 public class Tv {
 
-    String color;
-    boolean power; // 전원 상태
-    int channel; // 채널 번호
+//    String color;
+//    boolean power; // 전원 상태
+//    int channel; // 채널 번호
 
 }
