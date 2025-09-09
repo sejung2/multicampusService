45e4a4170b71166e0cef0e65e408e95d173af621
sejung2 <seingdev@gmail.com>
2025-09-09 22:05:45 +0900

    feat: servlet

---
+++ BackendClass/Servlet/src/main/java/com/seing/practice/servlet/HelloServlet.java
@@ -0,0 +1,35 @@
+package com.seing.practice.servlet;
+
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    /**
+     * @use Servlet#destroy()
+     * 생명주기 마지막에 호출되는 메서드
+     */
+    public void destroy() {
+        System.out.println("destroy() 호출");
+    }
+}
---
+++ BackendClass/Servlet/src/main/java/com/seing/practice/servlet/InputServlet.java
@@ -0,0 +1,72 @@
+package com.seing.practice.servlet;
+
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Serial;
+
+@WebServlet("/input")
+public class InputServlet extends HttpServlet {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InputServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see Servlet#init(ServletConfig)
+     */
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("init() 실행");
+    }
+
+    /**
+     * @see Servlet#destroy()
+     */
+    public void destroy() {
+        // TODO Auto-generated method stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String user_id = request.getParameter("user_id");
+        String user_pw = request.getParameter("user_pw");
+        String emailRcv = request.getParameter("emailRcv");
+        // 동일한 name 속성으로 값이 여러개가 전송되는 경우 배열로 받음
+        String[] subject = request.getParameterValues("subject");
+
+        System.out.println("id: " + user_id);
+        System.out.println("pw: " + user_pw);
+        System.out.println("emailRcv: " + emailRcv);
+
+        System.out.println("Selected subjects: ");
+        if (subject != null) {
+            for (String sub : subject) {
+                System.out.print(sub + " ");
+            }
+        } else {
+            System.out.println("No subjects selected");
+        }
+        System.out.println();
+    }
+
+}
