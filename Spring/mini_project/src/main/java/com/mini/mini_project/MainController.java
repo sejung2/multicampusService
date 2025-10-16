abf25db55f2a37314a155478f50f8fdaebce5ef9
sejung2 <seingdev@gmail.com>
2025-10-16 21:40:10 +0900

    feat: mini

---
+++ Spring/mini_project/src/main/java/com/mini/mini_project/MainController.java
@@ -0,0 +1,13 @@
+package com.mini.mini_project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
